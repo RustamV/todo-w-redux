748d22c9bc41f40321a5197825080a1d74b3ad64
emertarion <rvrd1996@gmail.com>
2020-05-25 18:27:23 +0500

    Updates

---
+++ precache-manifest.19749e32e4c31f9a71f2051d6c7a7163.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039a8f368f32136b7e45320d413f1336",
+    "url": "/todo-w-redux/index.html"
+  },
+  {
+    "revision": "e4f8326a436cfd39d3f6",
+    "url": "/todo-w-redux/static/css/main.7505a095.chunk.css"
+  },
+  {
+    "revision": "82ac27a54ba85e762556",
+    "url": "/todo-w-redux/static/js/2.4f0f3aba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-w-redux/static/js/2.4f0f3aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f8326a436cfd39d3f6",
+    "url": "/todo-w-redux/static/js/main.c866ef85.chunk.js"
+  },
+  {
+    "revision": "3ec8a14a246e85e7a5fb",
+    "url": "/todo-w-redux/static/js/runtime-main.afbba499.js"
+  },
+  {
+    "revision": "fcefd40b23f6bbd2b12bafdc46f90ac9",
+    "url": "/todo-w-redux/static/media/alarm.fcefd40b.svg"
+  },
+  {
+    "revision": "62855787f1d476f0e36984e7827e2b92",
+    "url": "/todo-w-redux/static/media/cancel.62855787.svg"
+  },
+  {
+    "revision": "f4972b2d563ca576d964a5230a0a56d3",
+    "url": "/todo-w-redux/static/media/close.f4972b2d.svg"
+  },
+  {
+    "revision": "474ff0c5323a146c14a94b573bbe2fa7",
+    "url": "/todo-w-redux/static/media/edit.474ff0c5.svg"
+  },
+  {
+    "revision": "80bc8f9074bfa0ad7c6bfc81ca543938",
+    "url": "/todo-w-redux/static/media/ellypsis-vertical.80bc8f90.svg"
+  },
+  {
+    "revision": "2395a275676aae60a9eb7cc606aff6f4",
+    "url": "/todo-w-redux/static/media/ok-circle.2395a275.svg"
+  }
+]);
